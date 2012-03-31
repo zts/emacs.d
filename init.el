c83a98919f27d6e32cf4b3656269b6f32e8d5dc0
Zachary Stevens <zts@cryptocracy.com>
2012-03-31 15:42:22 +0100

    Enable vendor directory.

--- init.el
+++ init.el
@@ -23,13 +23,13 @@
 ;; load vendor and custom files
 (defvar emacs-dir (file-name-directory load-file-name)
   "top level emacs dir")
-;; (defvar vendor-dir (concat emacs-dir "vendor/")
-;;   "Packages not yet available in ELPA")
+(defvar vendor-dir (concat emacs-dir "vendor/")
+  "Packages not yet available in ELPA")
 (defvar module-dir (concat emacs-dir "modules/")
   "Where the real configuration happens")
 
 ;; add to load path
-;; (add-to-list 'load-path vendor-dir)
+(add-to-list 'load-path vendor-dir)
 (add-to-list 'load-path module-dir)
 
 ;; require packages in modules/
