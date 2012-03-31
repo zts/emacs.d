d5600e45740d2a3a77fa8d26834907c44f6ecd47
Zachary Stevens <zts@cryptocracy.com>
2012-03-31 11:09:09 +0100

    Add js-comint, and make named functions pretty too.

--- modules/zts-javascript.el
+++ modules/zts-javascript.el
@@ -1,7 +1,17 @@
 (defun pretty-functions ()
   (font-lock-add-keywords
-   nil `(("\\(function *\\)("
+   nil `(("\\(function *\\).*("
           (0 (progn (compose-region (match-beginning 1)
                                     (match-end 1) "ƒ")
                     nil))))))
 (add-hook 'js-mode-hook 'pretty-functions)
+
+(require 'js-comint)
+(setq inferior-js-program-command "node")
+(setq inferior-js-mode-hook
+      (lambda ()
+        (ansi-color-for-comint-mode-on)
+        (add-to-list 'comint-preoutput-filter-functions
+                     (lambda (output)
+                       (replace-regexp-in-string ".*1G\.\.\..*5G" "..."
+                                                 (replace-regexp-in-string ".*1G.*3G" ">" output))))))
