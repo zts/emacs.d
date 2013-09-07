22811c4af4e856a303c3f2b6180153819eaa17c2
Zachary Stevens <zts@cryptocracy.com>
2013-09-07 22:05:05 +0100

    Start using ibuffer.

--- modules/zts-ido.el
+++ modules/zts-ido.el
@@ -11,3 +11,34 @@
 ;;      ido-use-virtual-buffers t
 ;;      ido-handle-duplicate-virtual-buffers 2
       ido-max-prospects 10)
+
+;; from http://emacs-fu.blogspot.co.uk/2010/02/dealing-with-many-buffers-ibuffer.html
+(require 'ibuffer)
+(setq ibuffer-saved-filter-groups
+  (quote (("default"
+            ("Org" ;; all org-related buffers
+              (mode . org-mode))
+            ("Mail"
+              (or  ;; mail-related buffers
+               (mode . message-mode)
+               (mode . mail-mode)
+               ;; etc.; all your mail related modes
+               ))
+            ("Elastera"
+              (filename . "code/elastera/"))
+            ("Adzuna"
+              (filename . "code/adzuna/"))
+            ("Code" ;; prog stuff not already in MyProjectX
+              (or
+                (mode . c-mode)
+                (mode . perl-mode)
+                (mode . python-mode)
+                (mode . emacs-lisp-mode)
+                ;; etc
+                ))
+            ("ERC"   (mode . erc-mode))))))
+
+(add-hook 'ibuffer-mode-hook
+  (lambda ()
+    (ibuffer-switch-to-saved-filter-groups "default")))
+(global-set-key (kbd "C-x C-b") 'ibuffer)
