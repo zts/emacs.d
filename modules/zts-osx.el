92980b22d9fc14eb78db1bb51585e4505395bf1d
Zachary Stevens <zts@cryptocracy.com>
2013-11-03 15:33:19 +0000

    Add user bindir to path.
    This fixes magit detecting the wrong "emacsclient".  There's certainly
    a neater way of making this change.

--- modules/zts-osx.el
+++ modules/zts-osx.el
@@ -1,3 +1,4 @@
 ;; Random bits that make everything work on OSX
 (if (eq system-type 'darwin)
-    (setq exec-path (cons "/usr/local/bin" exec-path)))
+    (setq exec-path (cons "/Users/zts/bin"
+                          (cons "/usr/local/bin" exec-path))))
