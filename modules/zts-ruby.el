692e61c8ab4495072aff6b5d1679af660e27734c
Zachary Stevens <zts@cryptocracy.com>
2014-07-27 18:29:45 +0100

    Use ruby-mode for various ruby DSLs

---
+++ modules/zts-ruby.el
@@ -0,0 +1,8 @@
+;; ruby-related stuff
+(add-to-list 'auto-mode-alist '("Gemfile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Berksfile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Guardfile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Rakefile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.ddl$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.gemspec$" . ruby-mode))
+
