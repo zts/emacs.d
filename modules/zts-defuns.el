e0a6a92eb43a182093a43acdedd440441a807978
Zachary Stevens <zts@cryptocracy.com>
2014-10-18 14:46:38 +0100

    Add `C-x y` to yank filename of current buffer

--- modules/zts-defuns.el
+++ modules/zts-defuns.el
@@ -18,3 +18,16 @@
     )
   )
 
+
+;; from http://emacsredux.com/blog/2013/03/27/copy-filename-to-the-clipboard/
+(defun copy-file-name-to-clipboard ()
+  "Copy the current buffer file name to the clipboard."
+  (interactive)
+  (let ((filename (if (equal major-mode 'dired-mode)
+                      default-directory
+                    (buffer-file-name))))
+    (when filename
+      (kill-new filename)
+      (message "Copied buffer file name '%s' to the clipboard." filename))))
+(global-set-key (kbd "C-x y") 'copy-file-name-to-clipboard)
+
