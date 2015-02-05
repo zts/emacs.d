4efb4ed9e66b240695f329393bb06b2029bdcfc9
Zachary Stevens <zts@cryptocracy.com>
2015-02-05 22:33:22 +0000

    Add melpa as a package source

--- init.el
+++ init.el
@@ -8,6 +8,8 @@
 (add-to-list 'package-archives
 	     '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (add-to-list 'package-archives
+             '("melpa" . "http://melpa.org/packages/") t)
+(add-to-list 'package-archives
 	     '("tromey" . "http://tromey.com/elpa/") t)
 
 (package-initialize)
