8a7155b7ae24e4fa29d548e68f395cd8085e8511
Zachary Stevens <zts@elastera.com>
2017-08-10 22:31:20 +0100

    Fix command environment path
    This fixed a problem where git commit hooks executed via magit could
    not find programs in `/usr/local/bin`

--- modules/zts-osx.el
+++ modules/zts-osx.el
@@ -3,4 +3,6 @@
     (setq exec-path (cons "/Users/zts/bin"
                           (cons "/usr/local/bin" exec-path)))
     (setq eshell-path-env (concat "/Users/zts/bin:/usr/local/bin" ":" eshell-path-env))
+    (let ((path (shell-command-to-string ". ~/.zshrc; echo -n $PATH")))
+      (setenv "PATH" path))
   )
